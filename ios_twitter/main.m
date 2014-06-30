d8b9b50bbabf33fce482d71bcb05c9d7c13b56bb
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-06-29 18:51:07 -0700

    Initial Commit

---
+++ ios_twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios_twitter
+//
+//  Created by Stanley Ng on 6/29/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ios_twitterTests/ios_twitterTests.m
@@ -0,0 +1,34 @@
+//
+//  ios_twitterTests.m
+//  ios_twitterTests
+//
+//  Created by Stanley Ng on 6/29/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios_twitterTests : XCTestCase
+
+@end
+
+@implementation ios_twitterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
