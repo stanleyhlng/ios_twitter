9f5811b80898aec7130b5bff12e5d397ff1e1dfa
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-07-03 16:21:35 -0700

    setup favorite

--- ios_twitter/TweetViewController.m
+++ ios_twitter/TweetViewController.m
@@ -8,12 +8,20 @@
 
 #import "TweetViewController.h"
 #import "ComposeViewController.h"
+#import "TwitterClient.h"
 
 @interface TweetViewController ()
 - (void)customizeRightBarButton;
 - (void)customizeTitleView;
 - (void)setupTweet;
+- (void)handleFavorite;
 - (void)handleReply;
+- (void)createFavoriteWithParam:(NSMutableDictionary *)params
+                        success:(void(^)(Tweet *tweet))success
+                        failure:(void(^)(NSError *error))failure;
+- (void)destroyFavoriteWithParam:(NSMutableDictionary *)params
+                        success:(void(^)(Tweet *tweet))success
+                        failure:(void(^)(NSError *error))failure;
 @end
 
 @implementation TweetViewController
@@ -63,6 +71,37 @@
     NSLog(@"setup tweet: %@", self.tweet);
 }
 
+- (void)handleFavorite
+{
+    NSLog(@"handle favorite");
+    /*
+     NSLog(@"tweet.favorited: %@", self.tweet.favorited);
+     NSMutableDictionary *params =
+     [@{
+     @"id": self.tweet.id
+     } mutableCopy];
+     
+     if ([self.tweet.favorited isEqualToNumber:[NSNumber numberWithInt:0]]) {
+     NSLog(@"create favorite");
+     
+     [self createFavoriteWithParam:params success:^(Tweet *tweet) {
+     NSLog(@"[FAVORITE] tweets: %@", tweet);
+     self.tweet = tweet;
+     
+     } failure:nil];
+     }
+     else {
+     NSLog(@"destory favorite");
+     
+     [self destroyFavoriteWithParam:params success:^(Tweet *tweet) {
+     NSLog(@"[UNFAVORITE] tweets: %@", tweet);
+     self.tweet = tweet;
+     
+     } failure:nil];
+     }
+     */
+}
+
 - (void)handleReply
 {
     NSLog(@"handle reply");
@@ -73,6 +112,40 @@
     UINavigationController *nvc = [[UINavigationController alloc] initWithRootViewController:vc];
     
     [self presentViewController:nvc animated:YES completion:nil];
+}
+
+- (void)createFavoriteWithParam:(NSMutableDictionary *)params
+                        success:(void(^)(Tweet *tweet))success
+                        failure:(void(^)(NSError *error))failure
+{
+    [[TwitterClient instance] createFavoriteWithParams:params
+                                               success:^(AFHTTPRequestOperation *operation, Tweet *tweet) {
+                                                   NSLog(@"success: %@", tweet);
+                                                   success(tweet);
+                                               }
+                                               failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                                                   NSLog(@"failure: %@", error);
+                                                   if (failure != nil) {
+                                                       failure(error);
+                                                   }
+                                               }];
+}
+
+- (void)destroyFavoriteWithParam:(NSMutableDictionary *)params
+                         success:(void(^)(Tweet *tweet))success
+                         failure:(void(^)(NSError *error))failure
+{
+    [[TwitterClient instance] destroyFavoriteWithParams:params
+                                                success:^(AFHTTPRequestOperation *operation, Tweet *tweet) {
+                                                    NSLog(@"success: %@", tweet);
+                                                    success(tweet);
+                                                }
+                                                failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                                                    NSLog(@"failure: %@", error);
+                                                    if (failure != nil) {
+                                                        failure(error);
+                                                    }
+                                                }];
 }
 
 
